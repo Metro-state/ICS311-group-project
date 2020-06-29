0f0ab1a261c238b8a130d096edc0762d4b0a0435
Misky Abshir <misky.abshir@gmail.com>
2020-06-29 17:21:20 -0500

    Update DDL.sql
    added people table

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -1,12 +1,14 @@
 INSERT INTO `movies`
     (`id`, `name_native`, `year_made`, `name_english`, `extended_data`, `people`, `songs`, `media_details`)
 VALUES
-    ('1', 'Joker', 2019, 'Joker', 'creepy', 'Tom Brady', 'Shallow', 'popular')
-;
+    ('1', 'Joker', 2019, 'Joker', 'creepy', 'Tom Brady', 'Shallow', 'popular'),
 
 INSET INTO `songs` 
   (`songID`, `song_name`, `lyrics`)
 VALUES 
-   ('2', 'Shallow', 'Far from shallow')
-;
+   ('2', 'Shallow', 'Far from shallow'),
+
+INSET INTO `people` 
+  (`id`, `Actor`, `Actress`, `Lyricist`, `Producer`, `Director`)
+  ;
 
