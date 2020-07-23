57ecee0f86147d2ff39f8c23aff0613155ebe9a2
Kathy <65127247+Kat-cell@users.noreply.github.com>
2020-07-22 20:08:55 -0500

    changed people.id to people_id

--- sql/omdb_sql_VIEW_queries.sql
+++ sql/omdb_sql_VIEW_queries.sql
@@ -1,15 +1,9 @@
-INSERT INTO `song_media` (`song_media_id`, `s_link`, `s_link_type`, `song_id`) VALUES
-(1, 'https://www.youtube.com/watch?v=moSFlvxnbgk', 'video', 1);
-INSERT INTO `song_people` (`song_id`, `people_id`, `role`) VALUES
-(1, 1, 'Supporting Acto');
-
 CREATE VIEW song_view 
 AS 
   SELECT songs.song_id, 
          songs.title, 
          song_media.song_media_id, 
          song_people.people_id, 
-         people.id, 
          movies.movie_id, 
          movies.english_name, 
          Group_concat(keyword) AS keyword 
@@ -19,7 +13,7 @@ AS
          LEFT OUTER JOIN song_people 
                       ON songs.song_id = song_people.people_id 
          LEFT OUTER JOIN people 
-                      ON songs.song_id = people.id 
+                      ON songs.song_id = people.people_id 
          LEFT OUTER JOIN movies 
                       ON songs.song_id = movies.movie_id 
          LEFT OUTER JOIN song_keywords 
